53432fdae4f7af53dc5d37a67d8c4e0ace0314cc
Ariel Conti <arielconti10@gmail.com>
2024-08-07 12:47:50 +0100

    fixed responsive layout and changes on pagination

--- src/components/planetList.tsx
+++ src/components/planetList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { index, Planet } from "@/server/planet";
 import { Pagination } from "@/types/general";
 import { PlanetCard } from "@/components/planetCard";
@@ -13,6 +13,8 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useState } from "react";
 
 type PlanetListProps = {
   initialPage: number;
@@ -20,62 +22,97 @@ type PlanetListProps = {
 };
 
 export function PlanetList({ initialPage, initialSearch }: PlanetListProps) {
-  const { data, isLoading, error } = useQuery<Pagination<Planet>>({
-    queryKey: ["planets", initialPage, initialSearch],
-    queryFn: () => index({ page: initialPage, search: initialSearch }),
-  });
+  const [page, setPage] = useState(initialPage);
 
-  if (isLoading) return <div className="text-center py-4">Loading...</div>;
-  if (error)
-    return (
-      <div className="text-center py-4 text-red-500">
-        An error occurred: {error.message}
-      </div>
-    );
+  const { data, isLoading, isError, error } = useQuery<Pagination<Planet>>({
+    queryKey: ["planets", page, initialSearch],
+    queryFn: () => index({ page, search: initialSearch }),
+    placeholderData: keepPreviousData,
+  });
 
   const { results, next, previous, count } = data || {};
   const totalPages = Math.ceil((count || 0) / 10);
 
+  function SkeletonCard() {
+    return (
+      <div className="space-y-3">
+        <div className="aspect-square overflow-hidden">
+          <Skeleton className="h-full xl:h-80 w-full xl:min-w-80 min-w-32" />
+        </div>
+        <div className="space-y-1 p-2">
+          <Skeleton className="h-5 w-full" />
+        </div>
+      </div>
+    );
+  }
+
+  const renderPlanetCards = () => {
+    if (isLoading) {
+      return Array(10)
+        .fill(0)
+        .map((_, index) => <SkeletonCard key={index} />);
+    }
+    return results?.map((planet) => (
+      <Link
+        className="w-full"
+        key={planet.id}
+        href={`/planet/${planet.id}`}
+        passHref
+      >
+        <PlanetCard planet={planet} />
+      </Link>
+    ));
+  };
+
+  if (isError) {
+    return (
+      <div className="text-center py-4 text-red-500">
+        An error occurred: {(error as Error).message}
+      </div>
+    );
+  }
+
   return (
-    <section className="mt-8 flex flex-col" data-testid="planet-list">
-      <div className="mb-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-        {results?.map((planet, key) => (
-          <Link
-            className="card"
-            key={key + 1}
-            href={`/planet/${key + 1}`}
-            passHref
-          >
-            <PlanetCard planet={planet} />
-          </Link>
-        ))}
+    <section className="mt-8 flex flex-col w-full" data-testid="planet-list">
+      <div className="mb-12 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {renderPlanetCards()}
       </div>
 
       <PaginationUI className="overflow-x-auto">
         <PaginationContent className="flex-wrap justify-center">
-          {previous && (
-            <PaginationItem>
-              <PaginationPrevious href={`/?page=${initialPage - 1}`}>
-                Previous
-              </PaginationPrevious>
-            </PaginationItem>
-          )}
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={() => setPage((old) => Math.max(old - 1, 1))}
+              disabled={page === 1 || isLoading}
+            >
+              Previous
+            </PaginationPrevious>
+          </PaginationItem>
 
           {Array.from({ length: totalPages }, (_, i) => i + 1).map((i) => (
             <PaginationItem key={i} className="hidden sm:inline-block">
-              <PaginationLink isActive={i === initialPage} href={`/?page=${i}`}>
+              <PaginationLink
+                isActive={i === page}
+                onClick={() => setPage(i)}
+                disabled={isLoading}
+              >
                 {i}
               </PaginationLink>
             </PaginationItem>
           ))}
 
-          {next && (
-            <PaginationItem>
-              <PaginationNext href={`/?page=${initialPage + 1}`}>
-                Next
-              </PaginationNext>
-            </PaginationItem>
-          )}
+          <PaginationItem>
+            <PaginationNext
+              onClick={() => {
+                if (next) {
+                  setPage((old) => old + 1);
+                }
+              }}
+              disabled={!next || isLoading}
+            >
+              Next
+            </PaginationNext>
+          </PaginationItem>
         </PaginationContent>
       </PaginationUI>
     </section>
